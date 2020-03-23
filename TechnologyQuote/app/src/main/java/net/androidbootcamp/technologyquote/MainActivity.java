77fe62ff6fa0650d99da9aa434570971cb1a017f
Trish <tcarti3@live.spcollege.edu>
2020-03-22 20:43:30 -0400

    3-20-20

---
+++ TechnologyQuote/app/src/main/java/net/androidbootcamp/technologyquote/MainActivity.java
@@ -0,0 +1,14 @@
+package net.androidbootcamp.technologyquote;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
