d5866bfa2dec400a05bf222816a96a2b12ba63bf
Trish <tcarti3@live.spcollege.edu>
2020-03-29 19:29:15 -0400

    Signed-off-by: Trish <tcarti3@live.spcollege.edu>

---
+++ YourSchoolApp/app/src/main/java/net/androidbootcamp/yourschoolapp/School.java
@@ -0,0 +1,14 @@
+package net.androidbootcamp.yourschoolapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class School extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_school);
+    }
+}
